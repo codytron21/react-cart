cab504019af5d4a8fcf1eb2e9e8e0096ee2e5d2b
rohan <rohannak2057@gmail.com>
2023-01-12 21:46:11 +0530

    adds header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import "./styles/app.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home"
+import Header from "./components/Header"
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
 
-    </div>
+    </BrowserRouter>
   );
 }
 
