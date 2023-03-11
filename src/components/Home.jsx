8c2157b2121259c9091932fe9bc015ea5a17d82d
rohan <rohannak2057@gmail.com>
2023-03-11 18:12:34 +0530

    final commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { toast } from "react-hot-toast";
+import { useDispatch } from "react-redux";
 const img1 =
   "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
 const img2 =
@@ -19,8 +20,11 @@ const Home = () => {
       id: "2",
     },
   ];
+  const dispatch = useDispatch();
+
   const addToCartHandler = (options) => {
-    console.log(options);
+    dispatch({ type: "addToCart", payload: options });
+    dispatch({ type: "calculatePrice" });
     toast.success("Item added to Cart.");
   };
   return (
