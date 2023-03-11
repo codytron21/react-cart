f70412407e7976548d618b33c9543202f5660113
rohan <rohannak2057@gmail.com>
2023-03-11 18:49:55 +0530

    refactored

---
+++ src/assets/dummyItems.js
@@ -0,0 +1,61 @@
+const img1 =
+    "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+const img2 =
+    "https://cdn.shopify.com/s/files/1/2428/5565/products/Neemans-HaleBlack-ReLive-Knits-Jogger-FrontRightLogo-Comfortable-Shoes_1024x.jpg?v=1662876260";
+const img3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRImXz3CFnAD1cwAeeYtlkHlYXM7Bx-9YBhmtivgEmVrg&usqp=CAU&ec=48600113"
+const img4 = "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71E5zB1qbIL._SL1500_.jpg";
+const img5 = "https://m.media-amazon.com/images/I/51HG99cW4QL._SY355_.jpg"
+const img6 = "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51HBom8xz7L._AC_UL480_FMwebp_QL65_.jpg"
+const img7 = "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71dTgFrCTCL._SX522_.jpg"
+const img8 = "https://images-eu.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/G/31/img18/PC/SmartHome/Revamp/PC/alexa_smart_home._CB499295205_.jpg"
+const productList = [
+    {
+        name: "Mac Book",
+        price: 10000,
+        imgSrc: img1,
+        id: "1",
+    },
+    {
+        name: "Black Shoes",
+        price: 50,
+        imgSrc: img2,
+        id: "2",
+    },
+    {
+        name: "smart watch",
+        price: 100,
+        imgSrc: img3,
+        id: "3",
+    },
+    {
+        name: "Iphone ",
+        price: 250,
+        imgSrc: img4,
+        id: "4",
+    },
+    {
+        name: "Drone",
+        price: 300,
+        imgSrc: img5,
+        id: "5",
+    },
+    {
+        name: "Airpods",
+        price: 70,
+        imgSrc: img6,
+        id: "6",
+    },
+    {
+        name: "Smart Tv",
+        price: 500,
+        imgSrc: img7,
+        id: "7",
+    },
+    {
+        name: "Alexa Home",
+        price: 150,
+        imgSrc: img8,
+        id: "8",
+    },
+];
+export default productList
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { useSelector } from "react-redux";
+import image from "../assets/cart.png";
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
   return (
     <nav>
-      <h2> Logo Here.</h2>
+      <img src={image} alt="logo" className="logoIcon" />
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/cart"}>
